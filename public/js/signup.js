b80d9aca0fe938f573200e43a79935e0e1eeca11
gregrolwes <gregory.rolwes@slu.edu>
2021-05-24 19:13:53 -0500

    Added POST statement to backend

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,19 +5,33 @@ const openForm = (e) => {
   overlay.classList.add("active");
 };
 const closeModals = (e) => {
-  const form = document.getElementById("form");
   const overlay = document.getElementById("overlay");
-  const confirmation = document.getElementById("confirmation");
-  confirmation.classList.remove("active");
-  form.classList.remove("active");
+  const modals = document.getElementsByClassName("modal");
+  for (modal of modals) {
+    modal.classList.remove("active");
+  }
   overlay.classList.remove("active");
 };
 const submitEmail = (e) => {
-  console.log(document.getElementById("email").value);
-  const confirmation = document.getElementById("confirmation");
-  confirmation.classList.add("active");
+  email = document.getElementById("email").value;
   const form = document.getElementById("form");
   form.classList.remove("active");
+  fetch("https://scholarly-backend.uc.r.appspot.com/leads", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: email, source: "landing page" }),
+  })
+    .then(() => {
+      const confirmation = document.getElementById("confirmation");
+      confirmation.classList.add("active");
+    })
+    .catch((error) => {
+      const confirmation = document.getElementById("failure");
+      confirmation.classList.add("active");
+    });
 };
 
 (() => {
